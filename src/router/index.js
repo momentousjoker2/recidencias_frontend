0e6a6fa1dfee4c3778cc8e54f9e4c08872f297fd
Cristian mezuca Moreno <momentousjoker@gmail.com>
2021-10-14 18:04:18 -0500

    Estructura
    Es la estructura que llevara el proyecto sin menu

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,17 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import index from '../views/index.vue'
+import About from '../views/About.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'index',
+    component: index
   },
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/About.vue')
-    }
+    component: About,
   }
 ]
 
