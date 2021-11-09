781edebd8a22e0c528999056789285beefe1ff79
Cristian mezuca Moreno <momentousjoker@gmail.com>
2021-11-08 22:38:03 -0600

    Menu-Update
    Se agregaron menus vacios como componentes

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import AprobarSolicitud from '../views/AprobarSolicitud.vue'
 import RegistroAlumnos from '../views/RegistroAlumnos.vue'
 import EnviarSolicitud from '../views/EnviarSolicitud.vue'
 import RegistroActividad from '../views/RegistroActividad.vue'
-import tipoProyecto from '../views/tipoProyecto.vue'
+import tipoProyecto from '../views/TipoProyecto.vue'
 import CalificarActividad from '../views/CalificarActividad.vue'
 import Consultas from '../views/Consultas.vue'
 
