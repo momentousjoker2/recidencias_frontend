1d4b37036104c821925b9acd967f61371a1ccdad
Cristian mezuca Moreno <momentousjoker@gmail.com>
2021-11-10 22:04:29 -0600

    Crear menu
    Se creo un menu general con las configuracion necesaria para mostrar todos los datos

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
-  <div>
-    <menuAdmin/>
-    <menuJefeDepartamento />
-    <menuCoordinadorCarrera/>
-    <menuVentanillaServiciosEscolares/>
-    <menuEstudiantes/>
-  </div>
+    <h1>{{$store.getters.typeRoles}}</h1>
+
+    <menuAdmin                        v-if="$store.getters.typeRoles==='Administrador'"/>
+    <menuJefeDepartamento             v-else-if="$store.getters.typeRoles==='JefeDepartamento'"/>
+    <menuCoordinadorCarrera           v-else-if="$store.getters.typeRoles==='Cordinador'"/>
+    <menuVentanillaServiciosEscolares v-else-if="$store.getters.typeRoles==='Ventanilla'"/>
+    <menuEstudiantes                  v-else-if="$store.getters.typeRoles==='Estudiante'"/>
+
 </template>
 
 <script>
@@ -15,15 +16,17 @@ import menuJefeDepartamento from "@/components/menuJefeDepartamento.vue";
 import menuCoordinadorCarrera from "@/components/menuCoordinadorCarrera.vue";
 import menuVentanillaServiciosEscolares from "@/components/menuVentanillaServiciosEscolares.vue";
 import menuEstudiantes from "@/components/menuEstudiantes.vue";
+import store from './store'
 
 export default {
   name: "home",
+  store,
   components: {
     menuAdmin,
     menuJefeDepartamento,
     menuCoordinadorCarrera,
     menuVentanillaServiciosEscolares,
     menuEstudiantes
-  },
+  },  
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,15 @@
 import { createStore } from 'vuex'
 
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        rol: "Cordinador"
+    },
+    mutations: {},
+    actions: {},
+    modules: {},
+    getters: {
+        typeRoles(state) {
+            return state.rol;
+        }
+    }
 })
